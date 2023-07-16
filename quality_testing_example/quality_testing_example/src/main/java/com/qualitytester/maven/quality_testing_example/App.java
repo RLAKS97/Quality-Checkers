0dc6d631d943d65a67b4039572383cf93ca41908
RAGHULKANNA <116964840+RLAKS97@users.noreply.github.com>
2023-07-16 12:34:18 -0400

    Add files via upload

---
+++ quality_testing_example/quality_testing_example/src/main/java/com/qualitytester/maven/quality_testing_example/App.java
@@ -0,0 +1,13 @@
+package com.qualitytester.maven.quality_testing_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
